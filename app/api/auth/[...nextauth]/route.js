b027cf0cf54b282e60695d841f2a06bedd9064cc
AhmedBenAbdallahDev <ahmedbenabdallahdev@gmail.com>
2025-07-19 04:29:37 +0100

    Add Prisma adapter and update schema for authentication models

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/prisma";
 
 const handler = NextAuth({
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID,
@@ -13,53 +15,3 @@ const handler = NextAuth({
 });
 
 export { handler as GET, handler as POST };
-// export { handler as GET, handler as POST };
-
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import GithubProvider from "next-auth/providers/github"
-
-// const handler = NextAuth({
-// 	providers: [
-// 		GoogleProvider({
-// 			clientId: process.env.GOOGLE_ID,
-// 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-// 		}),
-// 	],
-// 	secret: process.env.NEXTAUTH_SECRET,
-// 	callbacks: {
-// 		async session({ session }) {
-// 			// store the user id from MongoDB to session
-// 			const sessionUser = await User.findOne({
-// 				email: session.user.email,
-// 			});
-// 			session.user.id = sessionUser._id.toString();
-
-// 			return session;
-// 		},
-// 		async signIn({ account, profile, user, credentials }) {
-// 			try {
-// 				await connectToDB();
-
-// 				// check if user already exists
-// 				const userExists = await User.findOne({ email: profile.email });
-
-// 				// if not, create a new document and save user in MongoDB
-// 				if (!userExists) {
-// 					await User.create({
-// 						email: profile.email,
-// 						username: profile.name.replace(" ", "").toLowerCase(),
-// 						image: profile.picture,
-// 					});
-// 				}
-
-// 				return true;
-// 			} catch (error) {
-// 				console.log("Error checking if user exists: ", error.message);
-// 				return false;
-// 			}
-// 		},
-// 	},
-// });
-
-// export { handler as GET, handler as POST };
